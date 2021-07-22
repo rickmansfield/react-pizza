802cfd1042cf037c1e65fb312a9c413eaadacb89
Rick <rmansfield@realpeoplesratings.com>
2021-07-22 16:35:53 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// import React from "react";
+
+// const App = () => {
+//   return (
+//     <>
+//       <h1>Lambda Eats</h1>
+//       <p>You can remove this code and create your own header</p>
+//     </>
+//   );
+// };
+// export default App;
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Route, Link, Switch } from 'react-router-dom';
+
+export default function App () {
+  return (
+ 
+ 
+    <>
+      <h1>Lambda Eats</h1>
+      <p>You can remove this code and create your own header</p>
+    </>
+  );
+}
